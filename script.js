cbdb2584052378924e8ec13657e7146ce19b354f
crimsoncrows <anjelaalexene@gmail.com>
2026-06-12 20:51:44 +0800

    added script for password validation, fix other elements and pages soon

--- script.js
+++ script.js
@@ -1,4 +1,35 @@
+function checkPasswordMatch() {
+    const newPassword = document.getElementById('newPassword').value;
+    const confirmPassword = document.getElementById('confirmPassword').value;
+    const passwordError = document.getElementById('matchError');
+
+    if (confirmPassword && newPassword !== confirmPassword) {
+        passwordError.classList.remove('d-none');
+    } else {
+        passwordError.classList.add('d-none');
+    }
+}
+
+
+
+function togglePass(inputId, span) {
+    const input = document.getElementById(inputId);
+    const icon = span.querySelector('i');
+    if (input.type === 'password') {
+        input.type = 'text';
+        icon.classList.replace('bi-eye', 'bi-eye-slash');
+    } else {
+        input.type = 'password';
+        icon.classList.replace('bi-eye-slash', 'bi-eye');
+    }
+
+}
 
 
 
 
+    
+
+
+document.getElementById('confirmPassword').addEventListener('input', checkPasswordMatch);
+    
